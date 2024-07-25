982598f36f35cebf053fb2cef8ede9aeb71f25d2
yos2003 <158718649+yos2003@users.noreply.github.com>
2024-07-25 16:36:55 +0100

    delete btn added

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,44 @@
 import "./App.css";
 import { useState } from "react";
 
+type Task = {
+  id: number;
+  content: string;
+};
+
 function App() {
   const [inputValue, setInputValue] = useState("");
-  const [data,setData]=useState<string[]>([]);
+  const [data, setData] = useState<Task[]>([]);
 
-  const handleInputChange=(e)=>{
-    setInputValue(e.target.value)
-  }
-  const addtask=()=>{
-    setData([ ...data,inputValue]);
-    setInputValue('');
-  }
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+  const addtask = () => {
+    setData([...data, { id: Date.now(), content: inputValue }]);
+    setInputValue("");
+  };
+  const deleteTask = (id:number) => {
+    setData(data.filter(task => task.id !== id));
+  };
   return (
-    <>
-      <div className="box">
-        <h1>Todo-List App</h1>
-        <div>
-          <input type="text" value={inputValue} onChange={handleInputChange} />
-          <button onClick={addtask}>Add</button>
-        </div>
-        <ul>
-          {data.map((task,index)=>
-            <li key={index}>
-            <p>{task}</p>
+    <div className="box">
+      <h1>Todo-List App</h1>
+      <div>
+        <input type="text" value={inputValue} onChange={handleInputChange} />
+        <button onClick={addtask}>Add</button>
+      </div>
+      <ul>
+        {data.map((task) => (
+          <li key={task.id}>
+            <p>{task.content}</p>
             <div className="two-btns">
               <button>edit</button>
-              <button>delete</button>
+              <button onClick={()=>deleteTask(task.id)}>delete</button>
             </div>
           </li>
-          )}
-        </ul>
-      </div>
-    </>
+        ))}
+      </ul>
+    </div>
   );
 }
-
 export default App;
